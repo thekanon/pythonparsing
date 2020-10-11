6607902335af2fbc2760782a16729f54d49cdfe2
Doo Lee <dlengjs12@naver.com>
2020-10-12 02:28:32 +0900

    해커랭크파싱 로직 수정

--- DuouOLingo/my-app/src/App.js
+++ DuouOLingo/my-app/src/App.js
@@ -13,6 +13,8 @@ function NewsDataList(props) {
         return <div key={index}>
             <li id={"newsLi"+index+"_0"}>{number[0]}</li>
             <li id={"newsLi"+index+"_1"}>{number[1]}</li>
+            <li id={"newsLi"+index+"_1"}>{number[2]}</li>
+            <li id={"newsLi"+index+"_1"}>{number[3]}</li>
             <button id={"newsBtn"+index} onClick={handleClick}>번역</button><br /></div>
     });
     return (<ul id="ulId">{listItems}</ul>);
@@ -30,10 +32,17 @@ class App extends React.Component {
     callTranslate = async (e) => {
         // const response = await (await fetch('http://localhost:3000/viewNews')).json()
         var hardData = "1."+e.target.parentElement.getElementsByTagName("li")[0].textContent+"1."+e.target.parentElement.getElementsByTagName("li")[1].textContent
-        const response = await (await fetch(`http://localhost:3000/translate/${hardData}`)).json()
+        const response = await fetch('http://localhost:3000/translate/',{
+            method : "POST",
+            body:JSON.stringify({data:hardData}),
+            headers:{
+                'Content-Type': 'application/json'
+            }            
+        })
         // this.setState({ translateList: response.message.result.translatedText });
-        this.setState({ translateList: response.message.result.translatedText});
-        console.log(response.message.result.translatedText)
+        // this.setState({ translateList: response.message.result.translatedText});
+        // console.log(response.message.result.translatedText)
+        console.log(response)
     }    
     componentDidMount() {
         this.callAPI()
@@ -43,19 +52,24 @@ class App extends React.Component {
         // 1const response = await fetch('http://localhost:3000/translate')
         if(response.length == 0){
             var hardData = [
-                ["House Lawmakers Condemn Big Tech’s ‘Monopoly Power’ and Urge Their Breakups","In a report led by Democrats, lawmakers said Apple, Amazon, Google and Facebook needed to be checked and recommended they be restructured and that antitrust laws be reformed."],
-                ["12 Accusations in the Damning House Report on Amazon, Apple, Facebook and Google","Lawmakers said they found multiple problems with each of the four giant tech companies."],
-                ["Facebook Amps Up Its Crackdown on QAnon","The company said an earlier effort to curb the conspiracy movement’s growth didn’t properly address its increasing popularity."],
-                ["WeChat Unites and Divides in America","The app, which unites families but also spreads Chinese propaganda, faces a ban in the United States."],
-                ["Trump Moves to Tighten Visa Access for High-Skilled Foreign Workers","Four weeks before the election, the Trump administration has announced stricter rules for the H-1B visa program, which U.S. companies have long valued."],
-                ["No, the Coronavirus Is Not Like the Flu","Experts have said repeatedly that the coronavirus poses a far more serious threat than influenza viruses."],
-                ["Seeking a Partner for the End of the World","Anticipating ugly months to come, Americans are dating with an intention that some experts say that they haven’t seen before."],
-                ["Big Tech Was Their Enemy, Until Partisanship Fractured the Battle Plans","A House report on how to limit the reach of Apple, Amazon, Google and Facebook has been delayed as Democrats and Republicans split on remedies."]
+                ["House Lawmakers Condemn Big Tech’s ‘Monopoly Power’ and Urge Their Breakups","In a report led by Democrats, lawmakers said Apple, Amazon, Google and Facebook needed to be checked and recommended they be restructured and that antitrust laws be reformed.","",""],
+                ["12 Accusations in the Damning House Report on Amazon, Apple, Facebook and Google","Lawmakers said they found multiple problems with each of the four giant tech companies.","",""],
+                ["Facebook Amps Up Its Crackdown on QAnon","The company said an earlier effort to curb the conspiracy movement’s growth didn’t properly address its increasing popularity.","",""],
+                ["WeChat Unites and Divides in America","The app, which unites families but also spreads Chinese propaganda, faces a ban in the United States.","",""],
+                ["Trump Moves to Tighten Visa Access for High-Skilled Foreign Workers","Four weeks before the election, the Trump administration has announced stricter rules for the H-1B visa program, which U.S. companies have long valued.","",""],
+                ["No, the Coronavirus Is Not Like the Flu","Experts have said repeatedly that the coronavirus poses a far more serious threat than influenza viruses.","",""],
+                ["Seeking a Partner for the End of the World","Anticipating ugly months to come, Americans are dating with an intention that some experts say that they haven’t seen before.","",""],
+                ["Big Tech Was Their Enemy, Until Partisanship Fractured the Battle Plans","A House report on how to limit the reach of Apple, Amazon, Google and Facebook has been delayed as Democrats and Republicans split on remedies.","",""]
             ]
             this.setState({ newsList: hardData });
         }
-        else
+        else{
+            for(var i=0;i<response.length;i++){
+                response[i].push("")
+                response[i].push("")
+            }
             this.setState({ newsList: response });
+        }
         console.log(response)    
     }
     update() {
@@ -78,7 +92,7 @@ class App extends React.Component {
                 <main>
                     <div className="BBC-List">
                         <h1>Please write in Korean.</h1>
-                        <NewsDataList numbers={news} onClick={this.callTranslate} />
+                        <NewsDataList numbers={this.state.newsList} onClick={this.callTranslate} translateList={this.state.translateList} />
 
                         <ul >
                             <li>title</li>
