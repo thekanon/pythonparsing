f17a20df8228595f78e3b3ab25caad138817e29e
Doo Lee <dlengjs12@naver.com>
2020-10-19 23:48:58 +0900

    셔플 및 번역 로직 수정

--- DuouOLingo/my-app/src/App.js
+++ DuouOLingo/my-app/src/App.js
@@ -12,17 +12,28 @@ function NewsDataList(props) {
         console.log(props)
         props.onReset(e)
     }
+    function callInputButton(e){
+        props.onInput(e);
+    }
+    function viewAnswer(arr){
+        console.log(arr)
+        // var str = "";
+
+        // for(var i=0;i<arr.length;i++){
+        //     str += numbers
+        // }
+        return arr
+    }
     const listItems = numbers.map((number,index) =>{
+        var buttonList1 =""
+        var buttonList2 =""
         if(number[2].length > 0 && number[6][0]) {
-            var buttonList1 = number[2].map((number,index) =>{
-                return <button id={"shuffleBtn1_"+index} key={"shuffleBtn1_"+index}>{number}</button>
+            buttonList1 = number[2].map((number,index) =>{
+                return <button id={"shuffleBtn1_"+index} index={index} key={"shuffleBtn1_"+index} onClick={callInputButton}>{number}</button>
             });    
-            var buttonList2 = number[3].map((number,index) =>{
-                return <button id={"shuffleBtn2_"+index} key={"shuffleBtn2_"+index}>{number}</button>
+            buttonList2 = number[3].map((number,index) =>{
+                return <button id={"shuffleBtn2_"+index} index={index} key={"shuffleBtn2_"+index} onClick={callInputButton}>{number}</button>
             });
-        } else {
-            var buttonList1 =""
-            var buttonList2 =""
         }
         return <div key={index} index={index}>
             <li id={"newsLi"+index+"_0"}>{number[0]}</li>
@@ -31,7 +42,9 @@ function NewsDataList(props) {
             <div>{buttonList2}</div>
             <li id={"newsLi"+index+"_2"}>{number[6][1] ? number[4]: ""}</li>
             <li id={"newsLi"+index+"_3"}>{number[6][1] ? number[5]: ""}</li>
-            <button id={"newsBtn"+index} onClick={handleClick}>해석하기</button><button id={"resetBtn"+index} onClick={resetClick}>원본보기</button><br /></div>
+            <button id={"newsBtn"+index} onClick={handleClick}>해석하기</button><button id={"resetBtn"+index} onClick={resetClick}>원본보기</button><br />
+            <text>{viewAnswer(number[7])}</text>
+            </div>
     });
     return (<ul id="ulId">{listItems}</ul>);
 }
@@ -51,6 +64,8 @@ class App extends React.Component {
             newsList[6] : this row Flag
             newsList[6][0] : (true / false) show/hide translate shuffle title, description
             newsList[6][1] : (true / false) show/hide translate title, description
+
+            newsList[7] : user Input
         */
         this.state = {
             newsList: [],
@@ -78,10 +93,17 @@ class App extends React.Component {
         console.log(response)
         // console.log(response)
     }
+    callInputButton = async (e) => {
+        console.log(e)
+        var idx = parseInt(e.target.parentElement.parentElement.getAttribute("index"))
+        var newTranslateList = this.state.newsList;
+        newTranslateList[idx][7].push(e.target.textContent);
+        this.setState({ newsList: newTranslateList });
+    }
     callOrgData = async (e) => {
         var idx = parseInt(e.target.parentElement.getAttribute("index"))
         var newTranslateList = this.state.newsList;
-        if(newTranslateList[idx][2] == ""){
+        if(newTranslateList[idx][2] === ""){
             this.callTranslate(e)
         }
         newTranslateList[idx][6][1] = newTranslateList[idx][6][1] ? false :true
@@ -90,16 +112,16 @@ class App extends React.Component {
     callTranData = async (e) => {
         var idx = parseInt(e.target.parentElement.getAttribute("index"))
         var newTranslateList = this.state.newsList;
-        if(newTranslateList[idx][2] == ""){
+        if(newTranslateList[idx][2] === ""){
             this.callTranslate(e)
         }
         newTranslateList[idx][6][0] = newTranslateList[idx][6][0] ? false :true
+        newTranslateList[idx][7] = []
         this.setState({ newsList: newTranslateList });
     }
     shake_it_String(str){
         //Fisher–Yates shuffle 알고리즘
         var arr = str.split(" ");
-        var newArr = [];
         console.log(arr);
         for(var i=arr.length-1;i>=0;i--){
             var ranIdx = parseInt(Math.random()*i)
@@ -117,27 +139,27 @@ class App extends React.Component {
     callAPI = async () => {
         const response = await (await fetch('http://localhost:3000/viewNews')).json()
         // 1const response = await fetch('http://localhost:3000/translate')
-        if(response.length == 0){
+        if(response.length === 0){
             var hardData = [
-                ["House Lawmakers Condemn Big Tech’s ‘Monopoly Power’ and Urge Their Breakups","In a report led by Democrats, lawmakers said Apple, Amazon, Google and Facebook needed to be checked and recommended they be restructured and that antitrust laws be reformed.","","","","",[false,false]],
-                ["12 Accusations in the Damning House Report on Amazon, Apple, Facebook and Google","Lawmakers said they found multiple problems with each of the four giant tech companies.","","","","",[false,false]],
-                ["Facebook Amps Up Its Crackdown on QAnon","The company said an earlier effort to curb the conspiracy movement’s growth didn’t properly address its increasing popularity.","","","","",[false,false]],
-                ["WeChat Unites and Divides in America","The app, which unites families but also spreads Chinese propaganda, faces a ban in the United States.","","","","",[false,false]],
-                ["Trump Moves to Tighten Visa Access for High-Skilled Foreign Workers","Four weeks before the election, the Trump administration has announced stricter rules for the H-1B visa program, which U.S. companies have long valued.","","","","",[false,false]],
-                ["No, the Coronavirus Is Not Like the Flu","Experts have said repeatedly that the coronavirus poses a far more serious threat than influenza viruses.","","","","",[false,false]],
-                ["Seeking a Partner for the End of the World","Anticipating ugly months to come, Americans are dating with an intention that some experts say that they haven’t seen before.","","","","",[false,false]],
-                ["Big Tech Was Their Enemy, Until Partisanship Fractured the Battle Plans","A House report on how to limit the reach of Apple, Amazon, Google and Facebook has been delayed as Democrats and Republicans split on remedies.","","","","",[false,false]]
+                ["House Lawmakers Condemn Big Tech’s ‘Monopoly Power’ and Urge Their Breakups","In a report led by Democrats, lawmakers said Apple, Amazon, Google and Facebook needed to be checked and recommended they be restructured and that antitrust laws be reformed.","","","","",[false,false],[]],
+                ["12 Accusations in the Damning House Report on Amazon, Apple, Facebook and Google","Lawmakers said they found multiple problems with each of the four giant tech companies.","","","","",[false,false],[]],
+                ["Facebook Amps Up Its Crackdown on QAnon","The company said an earlier effort to curb the conspiracy movement’s growth didn’t properly address its increasing popularity.","","","","",[false,false],[]],
+                ["WeChat Unites and Divides in America","The app, which unites families but also spreads Chinese propaganda, faces a ban in the United States.","","","","",[false,false],[]],
+                ["Trump Moves to Tighten Visa Access for High-Skilled Foreign Workers","Four weeks before the election, the Trump administration has announced stricter rules for the H-1B visa program, which U.S. companies have long valued.","","","","",[false,false],[]],
+                ["No, the Coronavirus Is Not Like the Flu","Experts have said repeatedly that the coronavirus poses a far more serious threat than influenza viruses.","","","","",[false,false],[]],
+                ["Seeking a Partner for the End of the World","Anticipating ugly months to come, Americans are dating with an intention that some experts say that they haven’t seen before.","","","","",[false,false],[]],
+                ["Big Tech Was Their Enemy, Until Partisanship Fractured the Battle Plans","A House report on how to limit the reach of Apple, Amazon, Google and Facebook has been delayed as Democrats and Republicans split on remedies.","","","","",[false,false],[]]
             ]
             this.setState({ newsList: hardData });
         }
         else{
-            var viewFlagList = [];
             for(var i=0;i<response.length;i++){
                 response[i].push("")
                 response[i].push("")
                 response[i].push("")
                 response[i].push("")
                 response[i].push([false,false]);
+                response[i].push([])
             }
             this.setState({ newsList: response });
         }
@@ -163,7 +185,7 @@ class App extends React.Component {
                 <main>
                     <div className="BBC-List">
                         <h1>Please write in Korean.</h1>
-                        <NewsDataList numbers={this.state.newsList} onClick={this.callTranData} onReset={this.callOrgData} translateList={this.state.translateList} />
+                        <NewsDataList numbers={this.state.newsList} onInput={this.callInputButton} onClick={this.callTranData} onReset={this.callOrgData} translateList={this.state.translateList} />
 
                         <ul >
                             <li>title</li>
