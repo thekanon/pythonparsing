f238a915056800e13569e41de257dc10d3d43a3c
Doo Lee <dlengjs12@naver.com>
2020-10-21 23:28:55 +0900

    버튼 클릭 시 로우인덱스 추가

--- DuouOLingo/my-app/src/App.js
+++ DuouOLingo/my-app/src/App.js
@@ -11,8 +11,8 @@ function NewsDataList(props) {
     function resetClick(e) {
         props.onReset(e)
     }
-    function callInputButton(e,index){
-        props.onInput(e,index);
+    function callInputButton(e,index,rIdx){
+        props.onInput(e,index,rIdx);
     }
     function viewAnswer(arr,buttonArr){
         var str = "";
@@ -27,10 +27,10 @@ function NewsDataList(props) {
         var buttonList2 =""
         if(number[2].length > 0 && number[6][0]) {
             buttonList1 = number[2].map((number,index) =>{
-                return <button id={"shuffleBtn1_"+index} index={index} key={"shuffleBtn1_"+index} onClick={(e) => callInputButton(e,index)}>{number}</button>
+                return <button id={"shuffleBtn1_"+index} index={index} key={"shuffleBtn1_"+index} onClick={(e) => callInputButton(e,index,0)}>{number}</button>
             });    
             buttonList2 = number[3].map((number,index) =>{
-                return <button id={"shuffleBtn2_"+index} index={index} key={"shuffleBtn2_"+index} onClick={(e) => callInputButton(e,index)}>{number}</button>
+                return <button id={"shuffleBtn2_"+index} index={index} key={"shuffleBtn2_"+index} onClick={(e) => callInputButton(e,index,1)}>{number}</button>
             });
         }
         return <div key={index} index={index}>
@@ -41,7 +41,8 @@ function NewsDataList(props) {
             <li id={"newsLi"+index+"_2"}>{number[6][1] ? number[4]: ""}</li>
             <li id={"newsLi"+index+"_3"}>{number[6][1] ? number[5]: ""}</li>
             <button id={"newsBtn"+index} onClick={handleClick}>해석하기</button><button id={"resetBtn"+index} onClick={resetClick}>원본보기</button><br />
-            <text>{viewAnswer(number[7],number[2])}</text>
+            <div><text>{viewAnswer(number[7],number[2])}</text></div>
+            <div><text>{viewAnswer(number[8],number[3])}</text></div>
             </div>
     });
     return (<ul id="ulId">{listItems}</ul>);
@@ -92,14 +93,16 @@ class App extends React.Component {
         console.log(response)
         // console.log(response)
     }
-    callInputButton = async (e,index) => {
+    callInputButton = async (e,index,rIdx) => {
         var idx = parseInt(e.target.parentElement.parentElement.getAttribute("index"))
         var newTranslateList = this.state.newsList;
 
-        if(newTranslateList[idx][7].indexOf(index) != -1)
-            newTranslateList[idx][7].splice(newTranslateList[idx][7].indexOf(index), 1);
+        console.log(e)
+
+        if(newTranslateList[idx][7+rIdx].indexOf(index) != -1)
+            newTranslateList[idx][7+rIdx].splice(newTranslateList[idx][7+rIdx].indexOf(index), 1);
         else
-            newTranslateList[idx][7].push(index);
+            newTranslateList[idx][7+rIdx].push(index);
         this.setState({ newsList: newTranslateList });
     }
     callOrgData = async (e) => {
@@ -143,14 +146,14 @@ class App extends React.Component {
         // 1const response = await fetch('http://localhost:3000/translate')
         if(response.length === 0){
             var hardData = [
-                ["House Lawmakers Condemn Big Tech’s ‘Monopoly Power’ and Urge Their Breakups","In a report led by Democrats, lawmakers said Apple, Amazon, Google and Facebook needed to be checked and recommended they be restructured and that antitrust laws be reformed.","","","","",[false,false],[]],
-                ["12 Accusations in the Damning House Report on Amazon, Apple, Facebook and Google","Lawmakers said they found multiple problems with each of the four giant tech companies.","","","","",[false,false],[]],
-                ["Facebook Amps Up Its Crackdown on QAnon","The company said an earlier effort to curb the conspiracy movement’s growth didn’t properly address its increasing popularity.","","","","",[false,false],[]],
-                ["WeChat Unites and Divides in America","The app, which unites families but also spreads Chinese propaganda, faces a ban in the United States.","","","","",[false,false],[]],
-                ["Trump Moves to Tighten Visa Access for High-Skilled Foreign Workers","Four weeks before the election, the Trump administration has announced stricter rules for the H-1B visa program, which U.S. companies have long valued.","","","","",[false,false],[]],
-                ["No, the Coronavirus Is Not Like the Flu","Experts have said repeatedly that the coronavirus poses a far more serious threat than influenza viruses.","","","","",[false,false],[]],
-                ["Seeking a Partner for the End of the World","Anticipating ugly months to come, Americans are dating with an intention that some experts say that they haven’t seen before.","","","","",[false,false],[]],
-                ["Big Tech Was Their Enemy, Until Partisanship Fractured the Battle Plans","A House report on how to limit the reach of Apple, Amazon, Google and Facebook has been delayed as Democrats and Republicans split on remedies.","","","","",[false,false],[]]
+                ["House Lawmakers Condemn Big Tech’s ‘Monopoly Power’ and Urge Their Breakups","In a report led by Democrats, lawmakers said Apple, Amazon, Google and Facebook needed to be checked and recommended they be restructured and that antitrust laws be reformed.","","","","",[false,false],[],[]],
+                ["12 Accusations in the Damning House Report on Amazon, Apple, Facebook and Google","Lawmakers said they found multiple problems with each of the four giant tech companies.","","","","",[false,false],[],[]],
+                ["Facebook Amps Up Its Crackdown on QAnon","The company said an earlier effort to curb the conspiracy movement’s growth didn’t properly address its increasing popularity.","","","","",[false,false],[],[]],
+                ["WeChat Unites and Divides in America","The app, which unites families but also spreads Chinese propaganda, faces a ban in the United States.","","","","",[false,false],[],[]],
+                ["Trump Moves to Tighten Visa Access for High-Skilled Foreign Workers","Four weeks before the election, the Trump administration has announced stricter rules for the H-1B visa program, which U.S. companies have long valued.","","","","",[false,false],[],[]],
+                ["No, the Coronavirus Is Not Like the Flu","Experts have said repeatedly that the coronavirus poses a far more serious threat than influenza viruses.","","","","",[false,false],[],[]],
+                ["Seeking a Partner for the End of the World","Anticipating ugly months to come, Americans are dating with an intention that some experts say that they haven’t seen before.","","","","",[false,false],[],[]],
+                ["Big Tech Was Their Enemy, Until Partisanship Fractured the Battle Plans","A House report on how to limit the reach of Apple, Amazon, Google and Facebook has been delayed as Democrats and Republicans split on remedies.","","","","",[false,false],[],[]]
             ]
             this.setState({ newsList: hardData });
         }
@@ -161,6 +164,7 @@ class App extends React.Component {
                 response[i].push("")
                 response[i].push("")
                 response[i].push([false,false]);
+                response[i].push([])
                 response[i].push([])
             }
             this.setState({ newsList: response });
